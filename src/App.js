9125f4836d1073d54aa88ae698b8464bbb51ad41
Elena <olena168@gmail.com>
2022-12-11 22:55:01 +0300

    new commit from to-do-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Photo1 from './photo1.jpg';
+import Photo2 from './photo2.jpg';
+import { ToDoList } from './ToDoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='list'>
+      <div>
+        <div className='container'>
+          <img src={Photo1} width='300px' alt='pic'/>
+        </div>
+        <div className='container'>
+          <h1>To Do List</h1>
+        </div>
+        <ToDoList />
+        <div className='container'>
+          <img src={Photo2} width='300px' alt='pic'/>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/ToDoList.js
@@ -0,0 +1,70 @@
+import { Component } from "react";
+import Icon from './icon.png'
+import Swal from 'sweetalert2'
+
+export class ToDoList extends Component {
+    state = {
+        userInput: '',
+        toDoList: []
+    }
+
+    onChangeEvent(e) {
+        this.setState({userInput: e})
+    }
+
+    addItem(input) {
+        if (input === '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please, enter what do you want to do'})
+        } else {
+            let listArray = this.state.toDoList;
+            listArray.push(input);
+            this.setState({toDoList: listArray, userInput: ''});
+        }
+    }
+
+    deleteItem() {
+        let listArray = this.state.toDoList;
+        listArray = [];
+        this.setState({toDoList: listArray});
+    }
+
+    crossWord(event) {
+        const li = event.target;
+        li.classList.toggle('crossed');
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
+    }
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                <div className="container">
+                    <input type='text' 
+                    placeholder="What do you want to do?"
+                    onChange={(e) => {this.onChangeEvent(e.target.value)}}
+                    value={this.state.userInput} />
+                </div>
+                <div className="container">
+                    <button className="add btn" onClick={() => this.addItem (this.state.userInput)}>ADD</button>
+                </div>
+                <div className="container">
+                <ul>
+                    {this.state.toDoList.map((item, index) => (<li onClick={this.crossWord} key={index}> <img src={Icon} alt='icon' width='20px' /> {item}</li>))}
+                </ul>
+                </div>
+                <div className="container">
+                    <button className="delete btn" onClick={() => this.deleteItem()}>DELETE</button>
+                </div>
+                </form>
+                
+            </div>
+        )
+    }
+}
+
